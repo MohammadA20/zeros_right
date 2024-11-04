17e4a5a3c342b0778f84982bae1d84b7a9bed378
MohammadA20 <mohamadadada123@gmail.com>
2024-11-04 18:54:54 +0500

    Adding comments to the code

--- zeros.py
+++ zeros.py
@@ -3,14 +3,16 @@
 """
 
 ZEROES = []
-ADD = []
+ADD = [] # List to add the non-zero numbers
 
 def move_zeros(numbers):
+    # If the number is non-zero, then append to the list else when we finish adding the numbers, it will add zeros to the end of the list
     for move in range(len(numbers)):
         if numbers[move] != 0:
             ADD.append(numbers[move])
         else:
             ZEROES.append(numbers[move])
 
+# Calling the function
 move_zeros([1, 3, 0, 10, 2, 0, 0, 0, 33, 3, 4])
-print(ADD + ZEROES)
+print(ADD + ZEROES) #It will add the non-zero numbers with zeros to the list
